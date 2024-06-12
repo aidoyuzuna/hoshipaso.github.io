6de6130138ececcf6ff02a73aafcfab3cedb5f09
aidoyuzuna <hoshipaso+gi@gmail.com>
2024-06-12 20:31:43 +0900

    👍 deploy.yml周りを更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.hoshipaso.com",
-  base: "/",
+  base: '/',
   trailingSlash: "always",
   integrations: [
     tailwind(),
